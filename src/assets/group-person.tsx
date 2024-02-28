09a0334257af9bffdec8b8c9829f789d8b2e070f
Soontorn Pongsaensee <soontornp60@nu.ac.th>
2024-02-28 15:55:25 +0700

    Add GroupPerson component

---
+++ src/assets/group-person.tsx
@@ -0,0 +1,23 @@
+const GroupPerson = () => (
+  <svg
+    width="76"
+    height="76"
+    viewBox="0 0 76 76"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="38" cy="38" r="38" fill="#B28A4C" fill-opacity="0.2" />
+    <circle cx="38" cy="38" r="30" fill="#B28A4C" fill-opacity="0.2" />
+    <circle cx="38" cy="38" r="20" fill="#B28A4C" />
+    <path
+      d="M38 36C40.2091 36 42 34.2091 42 32C42 29.7909 40.2091 28 38 28C35.7909 28 34 29.7909 34 32C34 34.2091 35.7909 36 38 36Z"
+      fill="white"
+    />
+    <path
+      d="M46 43.5C46 45.985 46 48 38 48C30 48 30 45.985 30 43.5C30 41.015 33.582 39 38 39C42.418 39 46 41.015 46 43.5Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default GroupPerson;
