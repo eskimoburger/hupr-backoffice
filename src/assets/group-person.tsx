932f05051d51f8bfe92ed063c9d41712611ef7e8
Soontorn Pongsaensee <soontornp60@nu.ac.th>
2024-02-28 17:05:45 +0700

    Update SVG attributes and add new icons

--- src/assets/group-person.tsx
+++ src/assets/group-person.tsx
@@ -1,14 +1,14 @@
 const GroupPerson = () => (
   <svg
-    width="76"
-    height="76"
+    width={76}
+    height={76}
     viewBox="0 0 76 76"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="38" cy="38" r="38" fill="#B28A4C" fill-opacity="0.2" />
-    <circle cx="38" cy="38" r="30" fill="#B28A4C" fill-opacity="0.2" />
-    <circle cx="38" cy="38" r="20" fill="#B28A4C" />
+    <circle cx={38} cy={38} r={38} fill="#B28A4C" fillOpacity="0.2" />
+    <circle cx={38} cy={38} r={30} fill="#B28A4C" fillOpacity="0.2" />
+    <circle cx={38} cy={38} r={20} fill="#B28A4C" />
     <path
       d="M38 36C40.2091 36 42 34.2091 42 32C42 29.7909 40.2091 28 38 28C35.7909 28 34 29.7909 34 32C34 34.2091 35.7909 36 38 36Z"
       fill="white"
