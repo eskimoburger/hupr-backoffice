62687f88e3819dcac70fc7e25d4a849a486c270d
Soontorn Pongsaensee <soontornp60@nu.ac.th>
2024-03-12 19:16:20 +0700

    Add dayjs and react-tailwindcss-datepicker dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   prefix: "",
   theme: {
