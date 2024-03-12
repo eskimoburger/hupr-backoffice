de87e41335e51fd36f33a3b252935d159a310470
Soontorn Pongsaensee <soontornp60@nu.ac.th>
2024-03-12 21:12:47 +0700

    Fix to display only on content page

--- src/page/content/content.tsx
+++ src/page/content/content.tsx
@@ -7,14 +7,13 @@ import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import { Content, SelectFrequency, contentOptions } from "./create-content";
 import { useEffect, useMemo, useRef, useState } from "react";
-import { Framework } from "@/components/MutiSelect";
-import { Checkbox } from "@/components/ui/checkbox";
+
 import { ResponseFrequency } from "@/types/frequency";
-import { Plus } from "lucide-react";
 import { Device, ResponseDevice } from "@/types/device";
 import { Textarea } from "@/components/ui/textarea";
 import AsyncSelect from "react-select/async";
 import axios from "axios";
+
 const ContentDetail = () => {
   const { id } = useParams();
   const { data } = useSWR<ResponseContent>(`${baseURL}/message/${id}`, {
@@ -26,7 +25,7 @@ const ContentDetail = () => {
   const startTimeRef = useRef<HTMLInputElement>(null);
   const endDateRef = useRef<HTMLInputElement>(null);
   const endTimeRef = useRef<HTMLInputElement>(null);
-  const [checkValue, setCheckValue] = useState("Always");
+
   const [selectedDevices, setSelectedDevices] = useState<string[]>([]);
   const [frequency, setFrequency] = useState("");
   const [contentTypes, setContentTypes] = useState<Content[]>([
@@ -34,7 +33,7 @@ const ContentDetail = () => {
       type: "text",
     },
   ]);
-  const [, setCheckedAllDevices] = useState(false);
+
   const { data: frequencies } = useSWR<ResponseFrequency>(
     "https://api-beacon.adcm.co.th/api/receiving_freq",
     fetcher
@@ -54,8 +53,6 @@ const ContentDetail = () => {
       : [];
   }, [devices]);
 
-  console.log(modifiedDevices);
-
   useEffect(() => {
     if (data) {
       contentNameRef.current!.value = data.response_data.data.campaign_name;
@@ -66,11 +63,8 @@ const ContentDetail = () => {
       startTimeRef.current!.value = startDate.toTimeString().split(" ")[0];
       endDateRef.current!.value = endDate.toISOString().split("T")[0];
       endTimeRef.current!.value = endDate.toTimeString().split(" ")[0];
-      setCheckValue(messageConfig.cron === null ? "Specific Time" : "Always");
       setFrequency(messageConfig.recieving_freq_uuid);
       setSelectedDevices(messageConfig.device_uuid);
-      console.log(data.response_data.data.message);
-
       setContentTypes(
         data.response_data.data.message.map((message) => {
           if (message.type === "text") {
@@ -99,70 +93,6 @@ const ContentDetail = () => {
     }
   }, [data]);
 
-  const handleImageUploadAtIndex = async (
-    index: number,
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    if (!file) return;
-    const formData = new FormData();
-    formData.append("media", file as Blob);
-    const response = await axios.post(
-      "https://api-beacon.adcm.co.th/api/media/upload",
-      formData
-    );
-    setContentTypes((prevContentTypes) => {
-      const newContentTypes = [...prevContentTypes];
-      newContentTypes[index].originalContentUrl =
-        baseURL + response.data.response_data.data.original;
-      newContentTypes[index].previewImageUrl =
-        baseURL + response.data.response_data.data.original;
-      return newContentTypes;
-    });
-  };
-
-  const handleVideoUploadAtIndex = async (
-    index: number,
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    if (!file) return;
-    const formData = new FormData();
-    formData.append("media", file as Blob);
-    const response = await axios.post(
-      "https://api-beacon.adcm.co.th/api/media/upload",
-      formData
-    );
-    setContentTypes((prevContentTypes) => {
-      const newContentTypes = [...prevContentTypes];
-      newContentTypes[index].originalContentUrl =
-        baseURL + response.data.response_data.data.original;
-      newContentTypes[index].previewImageUrl =
-        baseURL + response.data.response_data.data.original;
-      return newContentTypes;
-    });
-  };
-
-  const handleTemplateUploadAtIndex = async (
-    index: number,
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    if (!file) return;
-    const formData = new FormData();
-    formData.append("media", file as Blob);
-    const response = await axios.post(
-      "https://api-beacon.adcm.co.th/api/media/upload",
-      formData
-    );
-    setContentTypes((prevContentTypes) => {
-      const newContentTypes = [...prevContentTypes];
-      newContentTypes[index].template!.columns[0].imageUrl =
-        baseURL + response.data.response_data.data.original;
-      return newContentTypes;
-    });
-  };
-
   const renderValueContentNew = (index: number) => {
     const value = contentTypes[index];
     switch (value.type) {
@@ -174,13 +104,7 @@ const ContentDetail = () => {
             </Label>
             <Textarea
               value={value.text}
-              onChange={(e) => {
-                setContentTypes((prevContentTypes) => {
-                  const newContentTypes = [...prevContentTypes];
-                  newContentTypes[index].text = e.target.value;
-                  return newContentTypes;
-                });
-              }}
+              readOnly
               className="active:outline-none"
               placeholder="พิมพ์ข้อความของคุณที่นี่"
               id={`message-${index}`}
@@ -191,22 +115,11 @@ const ContentDetail = () => {
       case "image":
         return (
           <div className=" w-full  items-center gap-1.5 mt-2">
-            <Label htmlFor={`uploadImage-${index}`} className="text-base">
-              <span>อัปโหลดรูปภาพของคุณ</span>
-            </Label>
-            <Input
-              onChange={(e) => {
-                handleImageUploadAtIndex(index, e);
-              }}
-              type="file"
-              id={`uploadImage-${index}`}
-              accept="image/*"
-            />
             {value.originalContentUrl && (
               <img
                 src={value.originalContentUrl}
                 alt="preview"
-                className="w-32 h-32 mt-2 rounded-md"
+                className="mt-2"
               />
             )}
           </div>
@@ -214,19 +127,8 @@ const ContentDetail = () => {
       case "video":
         return (
           <div className=" w-full  items-center gap-1.5 mt-2">
-            <Label htmlFor={`video-${index}`} className="text-base">
-              <span>อัปโหลดวิดีโอของคุณ</span>
-            </Label>
-            <Input
-              onChange={(e) => {
-                handleVideoUploadAtIndex(index, e);
-              }}
-              type="file"
-              id={`video-${index}`}
-              accept="video/mp4"
-            />
             {value.originalContentUrl && (
-              <video controls className="w-32 h-32 mt-2 rounded-md">
+              <video controls className="mt-2 rounded-md">
                 <source src={value.originalContentUrl} type="video/mp4" />
                 <track kind="captions" />
               </video>
@@ -241,9 +143,7 @@ const ContentDetail = () => {
               <span>อัปโหลดรูปภาพของคุณ</span>
             </Label>
             <Input
-              onChange={(e) => {
-                handleTemplateUploadAtIndex(index, e);
-              }}
+              readOnly
               type="file"
               id={`uploadImage-${index}`}
               accept="image/*"
@@ -262,43 +162,17 @@ const ContentDetail = () => {
     }
   };
 
-  const setContentTypeAtIndex = (index: number, type: Content["type"]) => {
-    setContentTypes((prevContentTypes) => {
-      const newContentTypes = [...prevContentTypes];
-      newContentTypes[index].type = type;
-
-      if (type === "text") {
-        newContentTypes[index].text = "";
-      } else if (type === "image" || type === "video") {
-        newContentTypes[index].originalContentUrl = "";
-        newContentTypes[index].previewImageUrl = "";
-      } else if (type === "template") {
-        newContentTypes[index].template = {
-          type: "image_carousel",
-          columns: [
-            {
-              imageUrl: "",
-              action: {
-                type: "uri",
-                uri: "",
-              },
-            },
-          ],
-        };
-      }
-
-      return newContentTypes;
-    });
-  };
-
   return (
     <div>
-      <h1 className="text-3xl font-bold text-[#B28A4C] mb-2 ">แก้ไขสื่อ</h1>
+      <h1 className="text-3xl font-bold text-[#B28A4C] mb-2 ">
+        รายละเอียดสื่อ
+      </h1>
       <div className=" w-full items-center gap-1.5">
         <Label htmlFor="contentName">
           <span>ชื่อคอนเทนต์</span>
         </Label>
         <Input
+          readOnly
           ref={contentNameRef}
           type="text"
           id="contentName"
@@ -319,32 +193,10 @@ const ContentDetail = () => {
             <RadioGroup
               name={`content-type-${index}`}
               options={contentOptions}
-              onChange={(value) => {
-                const contentType = value as
-                  | "text"
-                  | "image"
-                  | "video"
-                  | "template";
-                setContentTypeAtIndex(index, contentType);
-              }}
+              onChange={() => {}}
               selectedValue={contentTypes[index].type}
             />
             {renderValueContentNew(index)}
-            <div className="flex justify-end mt-1">
-              <button
-                disabled={contentTypes.length <= 1}
-                onClick={() => {
-                  setContentTypes((prevContentTypes) => {
-                    const newContentTypes = [...prevContentTypes];
-                    newContentTypes.splice(index, 1);
-                    return newContentTypes;
-                  });
-                }}
-                className="btn btn-error w-[120px] btn-sm text-base text-white mt-2 flex items-center justify-center"
-              >
-                ลบ
-              </button>
-            </div>
           </div>
           {contentTypes[index].type === "template" && (
             <div className="self-center">
@@ -353,13 +205,7 @@ const ContentDetail = () => {
               </Label>
               <Input
                 value={contentTypes[index]?.altText ?? ""}
-                onChange={(e) => {
-                  setContentTypes((prevContentTypes) => {
-                    const newContentTypes = [...prevContentTypes];
-                    newContentTypes[index].altText = e.target.value;
-                    return newContentTypes;
-                  });
-                }}
+                readOnly
                 type="text"
                 id="altText"
                 placeholder=" ข้อความแนบลิงค์"
@@ -387,18 +233,6 @@ const ContentDetail = () => {
           )}
         </div>
       ))}
-      <button
-        disabled={contentTypes.length >= 5}
-        onClick={() => {
-          setContentTypes((prevContentTypes) => [
-            ...prevContentTypes,
-            { type: "text" },
-          ]);
-        }}
-        className="btn btn-primary w-[120px] btn-sm text-base text-white mt-2 flex items-center justify-center"
-      >
-        <Plus size={18} /> เพิ่ม
-      </button>
 
       <div className="my-2" />
       <Label htmlFor="contentPeriod">
@@ -408,6 +242,7 @@ const ContentDetail = () => {
         <fieldset>
           <legend className="text-sm">วันที่เริ่มต้น</legend>
           <input
+            readOnly
             ref={startDateRef}
             onFocus={() => {
               if (startDateRef.current) startDateRef.current.type = "date";
@@ -424,6 +259,7 @@ const ContentDetail = () => {
         <fieldset>
           <legend className="text-sm">เวลาที่เริ่มต้น</legend>
           <input
+            readOnly
             ref={startTimeRef}
             onFocus={() => {
               if (startTimeRef.current) startTimeRef.current.type = "time";
@@ -439,6 +275,7 @@ const ContentDetail = () => {
         <fieldset>
           <legend className="text-sm">วันที่สิ้นสุด</legend>
           <input
+            readOnly
             onFocus={() => {
               if (endDateRef.current) endDateRef.current.type = "date";
             }}
@@ -455,6 +292,7 @@ const ContentDetail = () => {
         <fieldset>
           <legend className="text-sm">เวลาที่สิ้นสุด</legend>
           <input
+            readOnly
             onFocus={() => {
               if (endTimeRef.current) endTimeRef.current.type = "time";
             }}
@@ -467,47 +305,16 @@ const ContentDetail = () => {
             type="text"
           />
         </fieldset>
-        <div className=" flex gap-2">
-          <div className="flex items-center gap-2">
-            <input
-              className="radio radio-primary radio-sm"
-              type="radio"
-              id="always"
-              name="period"
-              value="Always"
-              checked={checkValue === "Always"}
-              onChange={() => setCheckValue("Always")}
-            />
-            <label className="label-text" htmlFor="always">
-              สม่ำเสมอ
-            </label>
-          </div>
-          <div className="flex items-center gap-4">
-            <input
-              className="radio radio-primary radio-sm"
-              type="radio"
-              id="specific"
-              name="period"
-              checked={checkValue === "Specific Time"}
-              value="Specific Time"
-              onChange={() => setCheckValue("Specific Time")}
-            />
-            <label className="label-text" htmlFor="specific">
-              เฉพาะช่วง
-            </label>
-          </div>
-        </div>
+        <div />
         <div />
         <div>
           <Label className="label-text">
             <span>ความถี่ในการรับ</span>
           </Label>
           <SelectFrequency
+            isDisabled
             value={frequency}
             frequencies={frequencies?.response_data.data ?? []}
-            handleSelect={(value) => {
-              setFrequency(value);
-            }}
           />
         </div>
         <div />
@@ -525,13 +332,7 @@ const ContentDetail = () => {
             value={modifiedDevices.filter((device) =>
               selectedDevices.includes(device.value)
             )}
-            onChange={(selected) => {
-              if (selected) {
-                setSelectedDevices(
-                  (selected as Framework[]).map((device) => device.value)
-                );
-              }
-            }}
+            isDisabled
             menuPlacement="auto"
             loadOptions={(_, callBack) => {
               axios
@@ -551,35 +352,9 @@ const ContentDetail = () => {
             }}
           />
         </div>
-        <div>
-          <Label className="label-text">
-            <div className="flex items-center space-x-2">
-              <Checkbox
-                id="all-devices"
-                onCheckedChange={(checked) => {
-                  setCheckedAllDevices(checked as boolean);
-                  setSelectedDevices(
-                    checked ? modifiedDevices.map((device) => device.value) : []
-                  );
-                }}
-              />
-              <label
-                htmlFor="all-devices"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                อุปกรณ์ทุกตัว
-              </label>
-            </div>
-          </Label>
-        </div>
+        <div />
       </div>
       <br />
-      <button
-        // onClick={handleCreateContent}
-        className="btn outline-none btn-sm  btn-primary text-white flex items-center"
-      >
-        บันทึก
-      </button>
     </div>
   );
 };
